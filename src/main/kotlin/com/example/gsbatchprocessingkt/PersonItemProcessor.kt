83e7ae06379b57ec1434a630f6c6c137cc55219a
sunakan <sunakan03@gmail.com>
2021-01-10 07:34:05 +0900

    Convert PersonItemProcessor.java => PersonItemProcessor.kt

---
+++ src/main/kotlin/com/example/gsbatchprocessingkt/PersonItemProcessor.kt
@@ -0,0 +1,22 @@
+package com.example.gsbatchprocessingkt
+
+import com.example.gsbatchprocessingkt.entity.Person
+import kotlin.Throws
+import org.slf4j.LoggerFactory
+import org.springframework.batch.item.ItemProcessor
+import java.lang.Exception
+
+class PersonItemProcessor : ItemProcessor<Person, Person> {
+    @Throws(Exception::class)
+    override fun process(person: Person): Person {
+        val firstName = person.firstName.toUpperCase()
+        val lastName = person.lastName.toUpperCase()
+        val transformedPerson = Person(firstName, lastName)
+        log.info("Converting ($person) into ($transformedPerson)")
+        return transformedPerson
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(PersonItemProcessor::class.java)
+    }
+}
