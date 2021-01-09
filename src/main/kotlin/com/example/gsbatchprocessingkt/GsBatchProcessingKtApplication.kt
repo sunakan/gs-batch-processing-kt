a7cf129e86d7ab06c1214293ad391c520e25fca0
sunakan <sunakan03@gmail.com>
2021-01-09 10:48:15 +0900

    Initialize by spring initalizer

---
+++ src/main/kotlin/com/example/gsbatchprocessingkt/GsBatchProcessingKtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gsbatchprocessingkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsBatchProcessingKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<GsBatchProcessingKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gsbatchprocessingkt/GsBatchProcessingKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gsbatchprocessingkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GsBatchProcessingKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
